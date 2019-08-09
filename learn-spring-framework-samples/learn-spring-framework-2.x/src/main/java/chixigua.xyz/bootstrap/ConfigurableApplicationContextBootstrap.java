b7500607e3cb3ebf83e8a605109b54b083c4abe1
yang2yang <197753119@qq.com>
2019-08-09 22:04:15 +0800

    spirng-framework-2.x

---
+++ learn-spring-framework-samples/learn-spring-framework-2.x/src/main/java/chixigua.xyz/bootstrap/ConfigurableApplicationContextBootstrap.java
@@ -0,0 +1,25 @@
+package chixigua.xyz.bootstrap;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConfigurableApplicationContextBootstrap {
+
+    static {
+        // 调整系统属性 "env"，实现 "name" bean 的定义切换
+        // envValue 可能来自于 "-D" 命令行启动参数
+        // 参数当不存在时，使用 "prod" 作为默认值
+        String envValue = System.getProperty("env", "prod");
+        System.setProperty("env", envValue);
+    }
+
+    public static void main(String[] args) {
+        // 定义 XML ApplicationContext
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/configurable-context.xml");
+        // "name" bean 对象
+        String value = (String) context.getBean("name");
+        // "name" bean 内容输出
+        System.out.println("Bean 'name' 的内容为：" + value);
+        // 关闭上下文
+        context.close();
+    }
+}
