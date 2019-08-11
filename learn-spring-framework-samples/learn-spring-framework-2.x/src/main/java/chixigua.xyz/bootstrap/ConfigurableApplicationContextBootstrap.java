591f01d88271b39c060448fb60114469d9a2577a
yang2yang <197753119@qq.com>
2019-08-11 09:25:43 +0800

    子模块创建完成

--- learn-spring-framework-samples/learn-spring-framework-2.x/src/main/java/chixigua.xyz/bootstrap/ConfigurableApplicationContextBootstrap.java
+++ learn-spring-framework-samples/learn-spring-framework-2.x/src/main/java/chixigua.xyz/bootstrap/ConfigurableApplicationContextBootstrap.java
@@ -8,12 +8,13 @@ public class ConfigurableApplicationContextBootstrap {
         // 调整系统属性 "env"，实现 "name" bean 的定义切换
         // envValue 可能来自于 "-D" 命令行启动参数
         // 参数当不存在时，使用 "prod" 作为默认值
-        String envValue = System.getProperty("env", "prod");
+        String envValue = System.getProperty("env", "dev");
         System.setProperty("env", envValue);
     }
 
     public static void main(String[] args) {
         // 定义 XML ApplicationContext
+        // 先留意下这个location的方式，不需要写classpath的前缀
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/configurable-context.xml");
         // "name" bean 对象
         String value = (String) context.getBean("name");
